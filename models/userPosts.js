b576419d340722f492c4d2c2c2c3d4a466565429
Mario Cortes <mcortes.1113@gmail.com>
2020-01-24 12:48:12 -0600

    added image caption to model

--- models/userPosts.js
+++ models/userPosts.js
@@ -2,10 +2,23 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var userPostsSchema = {
-    /*
-    
-    */
 
+    accountID: String,
+    postImageCaption: {
+        type: String,
+        validate: [({ length }) => length <= 140 , "140 characters max."]
+    },
+
+    postImageURL: {
+        type: String,  
+        required: true,
+    },
+
+    postedDate: {
+        type: Date,
+        default: Date.now
+    }
+    
 }
 
 var posts = mongoose.model("userPosts", userPostsSchema);
